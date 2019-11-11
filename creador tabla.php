0ef987e715bf7a25cfaf071c5dec582e462dcaa8
Xtoor9 <cristian.tortosa.ov@gmail.com>
2019-11-10 16:07:15 -0800

    v1 marc

---
+++ creador tabla.php
@@ -0,0 +1,7 @@
+CREATE TABLE agenda (id INT NOT NULL AUTO_INCREMENT, nombre CHAR(50),direccion CHAR(100), telefono CHAR(15), email CHAR(50), KEY (id) )\g
+
+INSERT INTO agenda VALUES (0, 'Juan Pérez', 'C/ Laguna, 15. Sevilla', '95.455.55.55', 'juan@agenda.com' )\g
+
+INSERT INTO agenda VALUES (1, 'Luis García', 'C/ Betis, 22. Cádiz', '95.655.66.33', 'luis@agenda.com' )\g
+
+INSERT INTO agenda VALUES (2, 'Carlos Rodríguez', 'C/ Sevilla, 6. Huelva', '95.113.22.77', 'carlos@agenda.com' )\g
