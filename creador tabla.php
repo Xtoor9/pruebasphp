9f90d741eccb487c2ebe28bcdd9867ca972b0127
daw2-tortosa20 <cristian.tortosa.ovgithub@gmail.com>
2019-11-27 13:29:09 +0100

    Zeling

--- creador tabla.php
+++ creador tabla.php
@@ -3,5 +3,5 @@ CREATE TABLE agenda (id INT NOT NULL AUTO_INCREMENT, nombre CHAR(50),direccion C
 INSERT INTO agenda VALUES (0, 'Juan Pérez', 'C/ Laguna, 15. Sevilla', '95.455.55.55', 'juan@agenda.com' )\g
 
 INSERT INTO agenda VALUES (1, 'Luis García', 'C/ Betis, 22. Cádiz', '95.655.66.33', 'luis@agenda.com' )\g
-
+assasasa
 INSERT INTO agenda VALUES (2, 'Carlos Rodríguez', 'C/ Sevilla, 6. Huelva', '95.113.22.77', 'carlos@agenda.com' )\gggghjfyyhjftuputamadreaaaaaaaaaappp
